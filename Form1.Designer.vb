b8870a5ff91db536333a64e837788346a23d7ba8
omid samsami <ksj1388@gmail.com>
2020-10-09 15:13:11 +0330

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(652, 120)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 296)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(329, 103)
+        Me.TextBox1.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(155, 114)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(348, 13)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(174, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(167, 115)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(348, 94)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(167, 115)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Location = New System.Drawing.Point(480, 215)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(167, 115)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 4
+        Me.PictureBox4.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(502, 374)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(95, 34)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 4000
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(668, 225)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(59, 20)
+        Me.TextBox2.TabIndex = 6
+        Me.TextBox2.Text = "25"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(668, 273)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(59, 20)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.Text = "25"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(734, 225)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "X"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(733, 273)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(14, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Y"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,177 @@
+﻿Imports System.Drawing.Imaging
+Imports Tesseract
+
+
+Public Class Form1
+
+
+    Public Function GrayScaleFilter(ByVal image As Bitmap) As Bitmap
+        Dim grayScale As Bitmap = New Bitmap(image.Width, image.Height)
+        Dim y As Int32 = 0
+        Do While (y < grayScale.Height)
+            Dim x As Int32 = 0
+            Do While (x < grayScale.Width)
+                Dim c As Color = image.GetPixel(x, y)
+                Dim gs As Int32 = CType(((c.R * 0.3) _
+                            + ((c.G * 0.59) _
+                            + (c.B * 0.11))), Int32)
+                grayScale.SetPixel(x, y, Color.FromArgb(gs, gs, gs))
+                x = (x + 1)
+            Loop
+
+            y = (y + 1)
+        Loop
+
+        Return grayScale
+    End Function
+
+
+
+    Public Function invertimg(bm As Bitmap) As Bitmap
+        '   bm = New Bitmap(PictureBox1.Image)
+        Dim X As Integer
+        Dim Y As Integer
+        Dim r As Integer
+        Dim g As Integer
+        Dim b As Integer
+
+        For X = 0 To bm.Width - 1
+            For Y = 0 To bm.Height - 1
+                r = 255 - (bm.GetPixel(X, Y)).R
+                g = 255 - bm.GetPixel(X, Y).G
+                b = 255 - bm.GetPixel(X, Y).B
+                '  MsgBox("r: " & r & "g: " & g & "b: " & b)
+
+                bm.SetPixel(X, Y, Color.FromArgb(r, g, b))
+            Next Y
+        Next X
+        Return bm
+    End Function
+    Dim r As Rectangle
+
+    Dim g As Graphics
+
+
+
+    Private Sub setBrightnes(ByVal Brightness As Single)
+        ' Brightness should be -1 (black) to 0 (neutral) to 1 (white)
+
+        Dim colorMatrixVal As Single()() = {
+           New Single() {1, 0, 0, 0, 0},
+           New Single() {0, 1, 0, 0, 0},
+           New Single() {0, 0, 1, 0, 0},
+           New Single() {0, 0, 0, 1, 0},
+           New Single() {Brightness, Brightness, Brightness, 0, 1}}
+
+        Dim colorMatrix As New ColorMatrix(colorMatrixVal)
+        Dim ia As New ImageAttributes
+
+        ia.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap)
+
+        g.DrawImage(imgt, r, 0, 0, imgt.Width, imgt.Height, GraphicsUnit.Pixel, ia)
+        PictureBox4.Refresh()
+
+    End Sub
+    Dim imgt As Image
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub analisy()
+
+        Dim pn1 As Bitmap = New Bitmap(PictureBox1.Image)
+
+        Dim pn2 As Bitmap = GrayScaleFilter(pn1)
+        PictureBox2.Image = pn2
+
+        PictureBox3.Image = invertimg(pn2)
+        imgt = PictureBox3.Image
+        PictureBox4.Image = New Bitmap(PictureBox3.Width, PictureBox3.Height, PixelFormat.Format32bppArgb)
+
+        g = Graphics.FromImage(PictureBox4.Image)
+        r = New Rectangle(0, 0, PictureBox4.Width, PictureBox4.Height)
+        g.DrawImage(imgt, r)
+        setBrightnes(0.3)
+
+        Dim ocrEngine = New TesseractEngine(".\tessdata", "eng", EngineMode.Default, False)
+        ocrEngine.SetVariable("tessedit_char_whitelist", "0123456789") ' If digit only
+        ' ocrEngine.SetVariable("edges_max_children_per_outline", "40")
+
+        Dim imageWithText As Image = PictureBox4.Image
+        ocrEngine.DefaultPageSegMode = PageSegMode.SparseText
+        Dim Page = ocrEngine.Process(imageWithText)
+
+        TextBox1.Text = page.GetText
+
+        '   MsgBox(Val(TextBox1.Text.ToString))
+    End Sub
+
+    Dim op As New System.Windows.Forms.OpenFileDialog
+
+
+    Dim x As String
+    Dim y As String
+    Private Sub CaptureScreen()
+
+        Dim graph As Graphics = Nothing
+        Try
+            '
+            Dim frmleft As System.Drawing.Point = Me.Bounds.Location
+            Dim screenWidth As Integer = Screen.PrimaryScreen.Bounds.Width
+            Dim screenHeight As Integer = Screen.PrimaryScreen.Bounds.Height
+            Dim tx As Integer = TextBox2.Text
+            Dim ty As Integer = TextBox3.Text
+            Dim bmp As New Bitmap(tx, ty)
+
+            graph = Graphics.FromImage(bmp)
+            ' x = screenWidth - 207
+            ' y = screenHeight - (screenHeight - 200)
+            '  Label1.Text = "X: " & MousePosition.X & "  Y: " & MousePosition.Y
+            x = MousePosition.X
+            y = MousePosition.Y
+            graph.CopyFromScreen(x, y, 0, 0, bmp.Size)
+            ' graph.CopyFromScreen(screenWidth - 207, screenHeight - (screenHeight - 200), 0, 0, bmp.Size)
+
+            '   PictureBox4.Image = bmp
+            ' PictureBox1.Image = GrayScaleFilter(bmp)
+            Dim bmt As Bitmap = New Bitmap(bmp, 100, 107)
+            PictureBox1.Image = bmt
+
+            bmp.Dispose()
+            graph.Dispose()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '  op.ShowDialog()
+
+        op.ShowDialog()
+
+        PictureBox1.Image = Image.FromFile(op.FileName)
+
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        CaptureScreen()
+        analisy()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
